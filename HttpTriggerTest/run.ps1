5b57854cf853e77a4aac9deefe24b2bb4820601e
Alessio De Castro <alessio.a.de.castro@avanade.com>
2022-03-02 15:18:05 +0100

    first commit

---
+++ HttpTriggerTest/run.ps1
@@ -0,0 +1,10 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+# Associate values to output bindings by calling 'Push-OutputBinding'.
+Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+    StatusCode = [HttpStatusCode]::OK
+    Body = $(Get-Module -ListAvailable | Select-Object Name, Path)
+})
